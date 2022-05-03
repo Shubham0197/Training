772d7eded14cb0a19634bd15125de3157fbcfedb
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-03 19:45:39 +0530

    Practiced Blocks-yield statement,Module-Include,Require statement and Mixins

--- Ruby/test.rb
+++ Ruby/test.rb
@@ -1,13 +1,19 @@
-END {
-    puts 'IT works in the end'
-}
-puts 'Hello World'
-=begin
-THis is inside of the comment
-multiline comment
-=end
 
-BEGIN {
-    puts 'Its works first'
-}
 
+$LOAD_PATH << '.'
+require "trig"
+
+class Decade
+include Week
+   no_of_yrs = 10
+   def no_of_months
+      puts Week::FIRST_DAY
+      number = 10*12
+      puts number
+   end
+end
+d1 = Decade.new
+puts Week::FIRST_DAY
+Week.weeks_in_month
+Week.weeks_in_year
+d1.no_of_months
