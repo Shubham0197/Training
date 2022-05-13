28259be77b7748dea2b2fd7528dd3aceb49de029
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-13 18:06:20 +0530

    Created module and function to check whether 2 strings are anagram or not for q7

---
+++ Ruby/Practice_Assignment/ques7.rb
@@ -0,0 +1,17 @@
+# 7. Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+module Anagram
+  def Anagram.anagram?(s,t)
+    hash_s = Hash.new
+    hash_t = Hash.new
+    (s.chars.uniq.sort).each { |char| hash_s[char] =   s.count(char) }
+    (t.chars.uniq.sort).each { |char| hash_t[char] =   t.count(char) }
+    print hash_s, hash_t, "\n"
+    if hash_s == hash_t
+      print "it is anagram", "\n"
+    end
+  end
+end
+
+#s = 'car'
+#t = 'rac'
+#anagram?(s, t)
