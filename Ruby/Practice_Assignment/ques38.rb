5c5c41fb8f17618b7fae05c8a5443de3ab604cb1
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-15 22:41:18 +0530

    Created funtion to find the starting and ending position of the target value in the given array for ques 38

---
+++ Ruby/Practice_Assignment/ques38.rb
@@ -0,0 +1,18 @@
+=begin
+38. Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a 
+given target value. 
+If target is not found in the array, return [-1, -1].
+=end
+# Question done using array have to do with hash ????
+module Range
+  def search_range_using_array(nums, target)
+    position = [-1, -1]
+    if nums.rindex { |element| element == target }
+      position[1] = nums.rindex { |element| element == target }
+    end
+    if nums.index { |element| element == target }
+      position[0] = nums.index { |element| element == target }
+    end
+    position
+  end
+end
