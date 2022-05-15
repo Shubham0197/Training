b7e2e2a5062ea16b8ee4938adf27ad0074ff69f7
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-16 00:41:15 +0530

    Created funtion to string that represents the kth largest integer in the given array for ques 11

---
+++ Ruby/Practice_Assignment/ques11.rb
@@ -0,0 +1,13 @@
+=begin
+11.You are given an array of strings nums and an 
+integer k. Each string in nums represents an integer without leading zeros. 
+Return the string that represents the kth largest integer in nums. 
+Note: Duplicate numbers should be counted distinctly. For example, if nums is ["1","2","2"], "2" is the first 
+largest integer, "2" is the second-largest integer, and "1" is the third-largest integer.
+=end
+def kth_largest_number(nums, k)
+  array_int = []
+  nums.each { |string| array_int << string.to_i }
+  array_int.sort!{ |a, b| b <=> a }
+  return array_int[k - 1].to_s
+end
