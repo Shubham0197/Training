15cc2838a289ef6a1f15045ba6dcb41f771b1e5f
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-16 20:49:22 +0530

    Studied and practiced Lambdas in Ruby

--- Ruby/Programs/read_csv.rb
+++ Ruby/Programs/read_csv.rb
@@ -3,11 +3,11 @@ print CSV.read("test.csv")
 
 csvfile = File.new("test.csv", "r+")
 arr_line = []
-arr = IO.readlines("test.csv",chomp: true)
+arr = IO.readlines("test.csv", chomp: true)
 
 #duplicating the values
 for i in 0...arr.size
   arr_line << arr[i].split(",")
 end
 
-print arr_line,"\n"
+print arr_line, "\n"
