4494f44ce6e347c18906c5239d8a6d895175a6e9
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-17 16:40:30 +0530

    Pracaticed map method on array

---
+++ Ruby/Programs/map_practice.rb
@@ -0,0 +1,24 @@
+=begin
+Map useful for creating array out of the object which is enumerable thorugh 
+appling some logic in the process for manipulation
+=end
+def make(x)
+  final_result = []
+  x.times do |i|
+    row_elements = []
+    (i + 1).times do |j|
+      row_elements << i + 1
+    end
+    final_result << row_elements    
+  end
+  final_result + final_result.reverse.last(x-1)
+end
+
+array = make(5)
+
+print array,"\n"
+
+sum_lines = array.map{|line| line.sum()}
+# here used map to on array which is enumerable on each element in it
+# and replcaing the need to use each loop
+print sum_lines,"\n"
