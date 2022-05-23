e4230b43d352bf12ca319871bc1980563f097561
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-23 22:33:10 +0530

    Changing the output of the running migrations in rails

--- Ruby/Rails/airport/db/schema.rb
+++ Ruby/Rails/airport/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_163829) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_170125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_163829) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_products_on_name"
   end
 
   add_foreign_key "certifieds", "aircrafts"
