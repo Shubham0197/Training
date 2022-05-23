f4b9aa2e8f696e095c28c3d0d922447b7f9bfc74
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-23 22:25:43 +0530

    Using ececute method to directly execute sql query in rails console and praticing how to seed data

---
+++ Ruby/Rails/airport/db/migrate/20220523164336_seed_data_in_the_table_try2.rb
@@ -0,0 +1,11 @@
+class SeedDataInTheTableTry2 < ActiveRecord::Migration[7.0]
+  def up
+    5.times do |i|
+      Country.create(name: "Product ##{i}", citiy: "A product." )
+    end
+  end
+
+  def down
+    Country.delete_all
+  end
+end
