7e4564508efd0b71c15738f4897f43d43e1d8e25
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-24 20:03:59 +0530

    Reversible practice and Irreversible migrations raise error

--- Ruby/Rails/ecommerce/db/schema.rb
+++ Ruby/Rails/ecommerce/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_24_135739) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_24_142535) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_135739) do
     t.text "name"
     t.string "mobile_number"
     t.bigint "zipcode"
+    t.integer "sku"
   end
 
   create_table "customrs", force: :cascade do |t|
