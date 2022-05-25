f151a72ff6764c4816d597419f3c05142685cb97
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-25 22:20:01 +0530

    Using seed commmand to insert data

--- Ruby/Rails/ecommerce/db/seeds.rb
+++ Ruby/Rails/ecommerce/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+100000.times do |i|
+  ProductOption.create(product_option: "Prodcut#{i}", product_id: i)
+end
