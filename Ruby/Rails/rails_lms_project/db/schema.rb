d9654005b18c0b7409964282fb6de8aa04f9285b
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-25 17:03:12 +0530

    Started rails associations and practiced belongs to associations

--- Ruby/Rails/rails_lms_project/db/schema.rb
+++ Ruby/Rails/rails_lms_project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_110356) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_25_093233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_110356) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.text "name"
+    t.text "email_id"
+    t.text "mobile_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "book_authors", "authors"
