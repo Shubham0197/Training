bff8f021031b695c0db9789fbe9a59083a3651f2
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-27 20:46:42 +0530

    Comparison validator example practice

--- Rails/ecommerce/app/models/wallet.rb
+++ Rails/ecommerce/app/models/wallet.rb
@@ -1,4 +1,7 @@
 class Wallet < ApplicationRecord
   validates :name, presence: true
+  validates :name, confirmation: {case_sensitive: false}
+  validates :name_confirmation, presence: true
   validates :available, acceptance: {accept: 1 ,message: "Accept this"}
+  validates :amount, confirmation: true
 end
