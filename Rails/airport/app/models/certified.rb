cd99e905c74b9e0effcd9a5ef29ef90ad36cd147
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-31 15:22:08 +0530

    Practiced uniqueness validation in rails

--- Rails/airport/app/models/certified.rb
+++ Rails/airport/app/models/certified.rb
@@ -1,5 +1,5 @@
 class Certified < ApplicationRecord
   validates :employee_id, numericality: { odd: true, less_than_or_equal_to: 20}
   validates :aircraft_id, numericality: { even: true, greater_than: 2, only_integer: true}
-
+  validates :aircraft_id, uniqueness: {scope: :employee_id}
 end
