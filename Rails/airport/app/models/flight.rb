3e016b4fb94af74c00b1c67a536ede0e58b1a21d
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-31 21:11:55 +0530

    Commmon validation onption in rails practice

--- Rails/airport/app/models/flight.rb
+++ Rails/airport/app/models/flight.rb
@@ -1,7 +1,7 @@
 class Flight < ApplicationRecord
-  validates :from, length: { minimum: 2 ,too_short: "%{count} characters is the maximum allowed" }
+  validates :from, length: { minimum: 2 ,too_short: "%{count} characters is the maximum allowed" }, allow_nil: true
   validates :from, length: { maximum: 12 ,  too_long: "%{count} characters is the maximum allowed" }
-  validates :destination, length: { in: 2..12 }
+  validates :destination, length: { in: 2..12 }, allow_blank: true
   validates :price, length: { is: 5, wrong_length: "you gave wrong length" }
   validates :price, numericality: true
   validates :price, numericality: {in: 10000..59999, other_than: 29999}
--- Rails/airport/app/models/product.rb
+++ Rails/airport/app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  validates :name, length: { maximum: 12 }, on: :create
+  validates :description, length: { maximum: 12 }, on: :account_setup
+  validates :sku, length: { maximum: 4 }, on: :update
+  
   validate do |product|
     if name == description
       errors.add :description, :too_plain, message: "is same as product"
