ac133063ea95d617faa73b4c5766dc0b83115de0
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-31 14:54:57 +0530

    Practiced numericality validation in rails

--- Rails/airport/app/models/certified.rb
+++ Rails/airport/app/models/certified.rb
@@ -1,2 +1,5 @@
 class Certified < ApplicationRecord
+  validates :employee_id, numericality: { odd: true, less_than_or_equal_to: 20}
+  validates :aircraft_id, numericality: { even: true, greater_than: 2, only_integer: true}
+
 end
--- Rails/airport/app/models/flight.rb
+++ Rails/airport/app/models/flight.rb
@@ -3,6 +3,8 @@ class Flight < ApplicationRecord
   validates :from, length: { maximum: 12 ,  too_long: "%{count} characters is the maximum allowed" }
   validates :destination, length: { in: 2..12 }
   validates :price, length: { is: 5, wrong_length: "you gave wrong length" }
+  validates :price, numericality: true
+  validates :price, numericality: {in: 10000..59999, other_than: 29999}
 end
 
 # one flight can have many aircrfts on that routes
