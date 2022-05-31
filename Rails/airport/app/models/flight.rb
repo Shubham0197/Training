18083fa96fe635c98a3b776abd4b482e7d5eed3f
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-31 14:21:19 +0530

    Practiced length validation in rails

--- Rails/airport/app/models/flight.rb
+++ Rails/airport/app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
-  has_many :flights_aircrafts
-  has_many :aircrafts, through: :flights_aircrafts
+  validates :from, length: { minimum: 2 ,too_short: "%{count} characters is the maximum allowed" }
+  validates :from, length: { maximum: 12 ,  too_long: "%{count} characters is the maximum allowed" }
+  validates :destination, length: { in: 2..12 }
+  validates :price, length: { is: 5, wrong_length: "you gave wrong length" }
 end
 
 # one flight can have many aircrfts on that routes
