2ae8680db6b0ddfe219d9a3fa8f3163abef4352c
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-06 15:39:24 +0530

    dependent option of belongs_to association

--- Rails/airport/app/models/aircraft.rb
+++ Rails/airport/app/models/aircraft.rb
@@ -1,10 +1,12 @@
 class Aircraft < ApplicationRecord
-  validates :name, confirmation: true, unless: ->{ name.blank? }
-  validates :name_confirmation, presence: true, if: Proc.new {|aircraft| !aircraft.name.blank? }
-  validates :cruising_range, presence: true, unless: :cruising_range?
+ 
+  has_many :certifieds
+  # validates :name, confirmation: true, unless: ->{ name.blank? }
+  #validates :name_confirmation, presence: true, if: Proc.new {|aircraft| !aircraft.name.blank? }
+  #validates :cruising_range, presence: true, unless: :cruising_range?
   
-  def cruising_range?
-    name.blank?
-  end
+  #def cruising_range?
+   # name.blank?
+  #end
 
 end
--- Rails/airport/app/models/certified.rb
+++ Rails/airport/app/models/certified.rb
@@ -1,5 +1,6 @@
 class Certified < ApplicationRecord
-  belongs_to :pilots, class_name: "Employee" , :foreign_key => :employee_id , counter_cache: :count_of_pilots
+  belongs_to :pilots, class_name: "Employee" , :foreign_key => :employee_id , counter_cache: :count_of_pilots, dependent: :destroy
+  belongs_to :aircraft, dependent: :delete
   #validates :employee_id, numericality: { odd: true, less_than_or_equal_to: 20}
   #validates :aircraft_id, numericality: { even: true, greater_than: 2, only_integer: true}
   #validates :aircraft_id, uniqueness: {scope: :employee_id}
