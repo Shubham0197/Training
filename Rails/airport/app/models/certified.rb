e2895698fe31ca7473209afebe41a0b4c8d62249
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-06 13:46:42 +0530

    class_name and foreign key  option of belongs_to association

--- Rails/airport/app/models/certified.rb
+++ Rails/airport/app/models/certified.rb
@@ -1,5 +1,6 @@
 class Certified < ApplicationRecord
-  validates :employee_id, numericality: { odd: true, less_than_or_equal_to: 20}
-  validates :aircraft_id, numericality: { even: true, greater_than: 2, only_integer: true}
-  validates :aircraft_id, uniqueness: {scope: :employee_id}
+  belongs_to :pilots, class_name: "Employee" , :foreign_key => :employee_id
+  #validates :employee_id, numericality: { odd: true, less_than_or_equal_to: 20}
+  #validates :aircraft_id, numericality: { even: true, greater_than: 2, only_integer: true}
+  #validates :aircraft_id, uniqueness: {scope: :employee_id}
 end
