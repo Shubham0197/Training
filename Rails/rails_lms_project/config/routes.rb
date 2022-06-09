5026337d3e8b4c1b63fe310713f6cb4ee2b1380c
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-09 14:30:31 +0530

    Simple match routes and root routes in rails

--- Rails/rails_lms_project/config/routes.rb
+++ Rails/rails_lms_project/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'home/books'
+  get '/books', to: 'home#books'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
