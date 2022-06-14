492b35cc825c120429d9af1dbb13b4c8cd566fa0
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-14 16:09:20 +0530

    Active record Query Inteface scopes

--- Rails/ecommerce/app/models/category.rb
+++ Rails/ecommerce/app/models/category.rb
@@ -1,12 +1,14 @@
 class Category < ApplicationRecord
   validates :name , inclusion: {in: ["clothes", "electronics"]}
   validates :description, presence: true, if: :description?
+  default_scope { where(description: true) }
 
   def description?
     description =~ /[\w]+/
   end
 
   has_many :products
-  scope :cat, -> { where("name is not null ") }
+  scope :cat, -> { where("description = 0.0 ") }
+  scope :limit_num, ->(len = 1) {limit len}
 end
   
