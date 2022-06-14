84ce12a115eca40389b4ebd225da92d8fbf41bff
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-14 19:06:43 +0530

    Active record Callbacks creating an objects

--- Rails/ecommerce/app/models/wallet.rb
+++ Rails/ecommerce/app/models/wallet.rb
@@ -4,8 +4,20 @@ class Wallet < ApplicationRecord
 #  validates :name_confirmation, presence: true, if: ->{!name.blank?}
 #  validates :available, acceptance: {accept: 1 ,message: "Accept this"}
 #  validates :amount, confirmation: true, numericality: {greater_than: 90}
-  belongs_to :customr
+#  belongs_to :customr
   has_one :identification
+#  before_save :printing
+#  around_save :printing
+#  before_create :printing
+#  around_create :printing
+#  after_create :printing
+#  after_save :printing
+  after_commit :printing
+#  after_rollback :printing
+
+  def printing
+    print "Callback after_commit / after_rollback  \n "
+  end
 
   def transaction (amount)
     print amount, "deducted"
