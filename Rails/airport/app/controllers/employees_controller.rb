30ce39a419f1294601f5cc789485163747ecf37b
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-23 19:38:08 +0530

    action edit update, partial template and error messages

---
+++ Rails/airport/app/controllers/employees_controller.rb
@@ -0,0 +1,23 @@
+class EmployeesController < ApplicationController
+  def index
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- Rails/airport/config/routes.rb
+++ Rails/airport/config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'employees/index'
+  get 'employees/new'
+  get 'employees/create'
+  get 'employees/show'
+  get 'employees/edit'
+  get 'employees/update'
+  get 'employees/destroy'
   root 'aircraft#index'
   get 'aircraft/show'
   get 'aircraft/index'
   get 'aircraft/new'
  #  get 'aircraft/edit(:id)'
-  resources :aircraft
+  resources :aircraft, :employees
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
