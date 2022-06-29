cf270db4658a01c5c9c374c36c82248e2363213f
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-29 15:28:42 +0530

    Created About action, views and routes

--- Rails/question_bank/app/controllers/homepage_controller.rb
+++ Rails/question_bank/app/controllers/homepage_controller.rb
@@ -1,4 +1,9 @@
 class HomepageController < ApplicationController
   def index
+    
+  end
+
+  def about 
+
   end
 end
--- Rails/question_bank/config/routes.rb
+++ Rails/question_bank/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'homepage#index'
   get 'homepage/index'
+  get 'homepage/about', to: 'homepage#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
