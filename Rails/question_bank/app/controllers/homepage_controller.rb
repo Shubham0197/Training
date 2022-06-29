67f62b56774fa01b0bd861e5cd2144e690c6bf61
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-29 15:27:27 +0530

    Working with the flash messages

--- Rails/question_bank/app/controllers/homepage_controller.rb
+++ Rails/question_bank/app/controllers/homepage_controller.rb
@@ -1,6 +1,7 @@
 class HomepageController < ApplicationController
   def index
-    
+    flash[:notice] = "Logged in Successfully"
+    flash[:alert] = "Invalid email or password"
   end
 
   def about 
