333460efa15fc837a053b8fd4339448e3c1e58a7
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-30 19:07:14 +0530

    Password Reset Update

--- Rails/question_bank/app/controllers/password_resets_controller.rb
+++ Rails/question_bank/app/controllers/password_resets_controller.rb
@@ -13,4 +13,26 @@ class PasswordResetsController < ApplicationController
       redirect_to root_path, notice: "Sent the link to reset the password email not present "
     end
   end
+
+  def edit
+    @user = User.find_signed(params[:token], purpose: "password_reset")
+    # binding.irb to pause the code execution
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+      redirect_to sign_in_path, alert: "Password reset link has expired"
+  end
+
+  def update
+    @user = User.find_signed(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Password Reset Successfully"
+    else
+      render :edit
+    end
+  end
+
+  private
+    
+    def password_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
 end
