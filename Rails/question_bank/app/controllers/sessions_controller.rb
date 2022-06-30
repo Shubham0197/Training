e3b034c05ced557643640e864e821b28f175385e
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-30 13:08:43 +0530

    Sign in form created

--- Rails/question_bank/app/controllers/sessions_controller.rb
+++ Rails/question_bank/app/controllers/sessions_controller.rb
@@ -1,6 +1,25 @@
 class SessionsController < ApplicationController
+  def new
+
+  end
+  
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Logged in Successfully"
+    else
+      flash[:alert] = "Invalid Email or Password"
+      render :new
+    end
+  end
+  
   def destroy 
     session[:user_id] = nil
     redirect_to root_path, notice: "Logged Out"
   end
+  
+  private
+
+
 end
--- Rails/question_bank/config/routes.rb
+++ Rails/question_bank/config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "sign_up", to: "users#new"
   post "sign_up", to: "users#create"
   delete "logout", to: "sessions#destroy"
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
