6b37dc00b9c44c0a44bf0a40999a1a99b03c8a80
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-30 12:56:59 +0530

    Logout by clearing session user_id

--- Rails/question_bank/config/routes.rb
+++ Rails/question_bank/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'homepage/about', to: 'homepage#about'
   get "sign_up", to: "users#new"
   post "sign_up", to: "users#create"
+  delete "logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
