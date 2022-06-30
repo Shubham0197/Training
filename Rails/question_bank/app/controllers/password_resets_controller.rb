b71dc568bd506288f1aef631f0200715e2434f46
Shubham0197 <shubhamjain0197@gmail.com>
2022-06-30 18:17:47 +0530

    Action mailer and reset password token mailer

--- Rails/question_bank/app/controllers/password_resets_controller.rb
+++ Rails/question_bank/app/controllers/password_resets_controller.rb
@@ -7,8 +7,7 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_email(params[:email])
 
     if @user.present?
-      #send Email
-      #PasswordMailer.with(user :@user).reset.deliver_later
+      PasswordMailer.with(user: @user).reset.deliver_later
       redirect_to root_path, notice: "Sent the link to reset the password "
     else
       redirect_to root_path, notice: "Sent the link to reset the password email not present "
--- Rails/question_bank/config/routes.rb
+++ Rails/question_bank/config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
   get "password/reset", to: "password_resets#new"
   post "password/reset", to: "password_resets#create"
 
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
