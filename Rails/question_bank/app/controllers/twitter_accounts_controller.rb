4b4b82fa74c24003fa31365f3628f95559855d94
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-01 15:35:01 +0530

    Rails before action to set twitter accounts

--- Rails/question_bank/app/controllers/twitter_accounts_controller.rb
+++ Rails/question_bank/app/controllers/twitter_accounts_controller.rb
@@ -1,5 +1,6 @@
 class TwitterAccountsController <ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_twitter_account, only: [:destroy]
   def index 
     @twitter_accounts = Current.user.twitter_accounts
   end
@@ -10,4 +11,10 @@ class TwitterAccountsController <ApplicationController
     redirect_to twitter_accounts_path, notice: "Successfully disconnected @#{@twitter_account.username}"
   end
 
+  private
+
+  def set_twitter_account
+    @twitter_account = Current.user.twitter_accounts.find(params[:id])
+  end
+  
 end
