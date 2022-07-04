6035c19ea98382887cb544ab6b5da08a81924436
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-04 20:54:23 +0530

    Adding validation to tweets model and creating partial for the errors in the forms

--- Rails/question_bank/app/models/tweet.rb
+++ Rails/question_bank/app/models/tweet.rb
@@ -1,4 +1,18 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+
+  validates :body, length: { minimum: 1, maximum: 280 }
+  validates :publish_at, presence: true
+
+  after_initialize do
+    self.publish_at ||= 24.hours.from_now
+  end
+
+  after_save_commit do
+    if publish_at_previously_changed?
+      TweetJob.set(wait_until: publish_at).perform_later(self)
+    end
+  end
+
 end
