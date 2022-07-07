3c1b4736b6ca5e0212ab0d1caec64ca5cae91821
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-07 13:17:53 +0530

    Created User Model

--- Rails/assignment1/db/migrate/20220707072547_create_users.rb
+++ Rails/assignment1/db/migrate/20220707072547_create_users.rb
@@ -1,7 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.string :avatar
+      t.text   :about
+      t.string :password
+      t.string :state
       t.timestamps
     end
   end
