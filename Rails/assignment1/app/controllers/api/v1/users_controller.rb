3b7b940509093fdeceb17bd658beb051311a4fd6
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-07 17:06:51 +0530

    Created api for the user model

--- Rails/assignment1/app/controllers/api/v1/users_controller.rb
+++ Rails/assignment1/app/controllers/api/v1/users_controller.rb
@@ -2,23 +2,46 @@ class Api::V1::UsersController <ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    users = User.all.order(:id)
+    render json: {data: ActiveModelSerializers::SerializableResource.new(users, each_serializer: UserSerializer)}
   end
 
   def show
+    user = User.find(params[:id])
+    render json: {data: UserSerializer.new(user)}
   end
 
   def create
+    user = User.new user_params
+    if user.save
+      Sidekiq::Client.enqueue_to_in("default", Time.now + 2.seconds, MailWorker, user.email, user.first_name)
+      render json: {data: UserSerializer.new(user)}
+    else
+      render json: {data: user.errors}
+    end
   end
 
-  def update
 
+  def update
+    user = User.find(params[:id])
+    user.update user_params
+    if user.save
+      Sidekiq::Client.enqueue_to_in("default", Time.now + 2.seconds, MailWorker, user.email, user.first_name)
+      render json: {data: UserSerializer.new(user)}
+    else
+      render json: {data: user.errors}
+    end
   end
 
   def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: {data: "Successfully deleted  #{user.first_name}"}
   end
-
-  def twitter_account_params
+  
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :gender, :state, :avatar, :email, :password, :about)
   end
-
 
 end
--- Rails/assignment1/config/routes.rb
+++ Rails/assignment1/config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
