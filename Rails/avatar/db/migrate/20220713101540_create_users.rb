ac9e72519dc4ac915e9a61cdb9619aa3c517299f
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-13 16:10:17 +0530

    Created user model

--- Rails/avatar/db/migrate/20220713101540_create_users.rb
+++ Rails/avatar/db/migrate/20220713101540_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :gender
+      t.string :avatar
+      t.string :address
       t.timestamps
     end
   end
