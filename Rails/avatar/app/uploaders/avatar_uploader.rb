5f30aa3413fee9c17a6c2e46ccd5388be405be9d
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-13 19:27:46 +0530

    Add mini magic gem and using it it resize to create and show thumbnail of the image uploaded

--- Rails/avatar/app/uploaders/avatar_uploader.rb
+++ Rails/avatar/app/uploaders/avatar_uploader.rb
@@ -1,7 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -28,10 +28,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  #Create different versions of your uploaded files:
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
