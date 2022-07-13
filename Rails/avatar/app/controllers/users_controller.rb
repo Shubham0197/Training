9bc14b4d9a2c2b27d33c13ce854f7064246b5558
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-13 17:59:00 +0530

    Created action and views for index, signup and create

--- Rails/avatar/app/controllers/users_controller.rb
+++ Rails/avatar/app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all.order("id")
   end
 
   def signup
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Successfully Create Account"
+    else
+      flash[:alert] = "Details Entered Incorrectly"
+      render :signup
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :avatar, :address, :gender)
   end
 end
--- Rails/avatar/config/routes.rb
+++ Rails/avatar/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
-  post 'users/signup'
+  get 'users/signup'
+  post 'users/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
