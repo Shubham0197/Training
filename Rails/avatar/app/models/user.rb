8b939946d56a25aee3f9052c2641c2f8d65bd6e5
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-13 23:07:44 +0530

    Adding validation on the format and the size of the uploaded file

--- Rails/avatar/app/models/user.rb
+++ Rails/avatar/app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
+
+  validates_processing_of :image
+validate :image_size_validation
+ 
+private
+  def image_size_validation
+    errors[:image] << "should be less than 500KB" if image.size > 5.megabytes
+  end
 end
