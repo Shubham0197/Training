ec1cb4fefa13e941cc6dd24063b596b515e46be2
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-15 13:16:47 +0530

    Practicing Linux Commands

--- Rails/avatar/config/routes.rb
+++ Rails/avatar/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
   get 'users/signup'
-
+  post 'users/create'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
