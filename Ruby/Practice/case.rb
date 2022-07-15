1f0e3230d8751019f64d3f7b59193cc69e65c479
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-15 15:03:43 +0530

    program to lower case, upper case and capitalizes all the words

---
+++ Ruby/Practice/case.rb
@@ -0,0 +1,15 @@
+# Write a Ruby program to lower case, upper case and capitalizes all the words of a given string.
+str = gets.chomp
+
+print(str.upcase)
+print("\n",str.downcase)
+print("\n",str.capitalize)
+
+=begin
+ (base) itsacheckmate@itsacheckmate:~/Training/Ruby/Practice$ ruby case.rb 
+shubham
+SHUBHAM
+shubham
+Shubham(base) itsacheckmate@itsacheckmate:~/Training/Ruby/Practice$  
+  
+=end
