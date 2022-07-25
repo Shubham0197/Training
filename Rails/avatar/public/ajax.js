74a76262752bc47dde4fe4b4a908682cd9c81a0a
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-25 16:02:26 +0530

    Updating the list using ajax api get request

--- Rails/avatar/public/ajax.js
+++ Rails/avatar/public/ajax.js
@@ -12,7 +12,7 @@ function buttonClickHandler(){
   // open the object
   //xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
   //using this for the post request
-  xhr.open('GET', 'hhttps://dummy.restapiexample.com/api/v1/create', true);
+  xhr.open('POST', 'hhttps://dummy.restapiexample.com/api/v1/create', true);
   xhr.getResponseHeader('Content-type', 'application/json');
 
 
@@ -39,5 +39,44 @@ function buttonClickHandler(){
   xhr.send(params);
 
   console.log("We are ddone");
+
+}
+
+
+let popBtn = document.getElementById("popBtn");
+popBtn.addEventListener('click',popHandler);
+function popHandler(){
+  console.log("You hhave Clicket  pop the button");
+
+  //INstantiate an XHR object
+  const xhr = new XMLHttpRequest();
+
+  // open the object
+  //xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
+  //using this for the post request
+  xhr.open('GET', 'https://dummy.restapiexample.com/api/v1/employees', true);
+
+
+  // what to do when respoonse is ready
+  xhr.onload = function(){
+    if(this.status === 200 ) {
+    let obj = JSON.parse(this.responseText);
+    console.log(obj);
+    let list = document.getElementById('list');
+    str = "";
+    for (key in obj){
+      str+= `<li>${obj[key].employee_name}</li>`;
+
+    }
+    list.innerHTML = str ;
+    }
+    else{
+      console.error("Some Error Occured");
+    }
+  }
+
+  xhr.send();
+
+  console.log("We are ddone fetching the data");
 
 }
