42727e4043b682205d1ad94c5dde4cc43c9a087d
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-28 00:51:20 +0530

    Solved rails admin issue of webpacker and changing the css and js file used by the the gem

--- Rails/airport/app/javascript/application.js
+++ Rails/airport/app/javascript/application.js
@@ -1,10 +1,1 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "trix"
-
-import "@rails/actiontext"
-import * as bootstrap from "bootstrap"
-import Rails from "@rails/ujs"
-Rails.start()
-
--- Rails/airport/config/environments/development.rb
+++ Rails/airport/config/environments/development.rb
@@ -1,8 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -69,5 +67,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
