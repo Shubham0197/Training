35019f753ca23f34faeeef1cd81d51b25d87c4c8
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-31 23:58:36 +0530

    aircraft association to schedule defined

--- Rails/airport/app/models/aircraft.rb
+++ Rails/airport/app/models/aircraft.rb
@@ -3,6 +3,7 @@ class Aircraft < ApplicationRecord
   #self.primary_key = 'name'
  # before_create :aborting
   has_many :certifieds, before_add: :check_ceritfieds
+  has_many :schedules
   # validates :name, confirmation: true, unless: ->{ name.blank? }
   #validates :name_confirmation, presence: true, if: Proc.new {|aircraft| !aircraft.name.blank? }
   #validates :cruising_range, presence: true, unless: :cruising_range?
