d20e2bccb905c3a16c073e376ba91e339182af96
Shubham0197 <shubhamjain0197@gmail.com>
2022-07-31 23:59:51 +0530

    Flight association to schedule defined and enum for the status created

--- Rails/airport/app/models/flight.rb
+++ Rails/airport/app/models/flight.rb
@@ -1,10 +1,11 @@
 class Flight < ApplicationRecord
+  has_many :schedules
+  enum :status, [ :NotScheduled, :Scheduled, :Completed, :Cancelled ]
+
   validates :from, length: { minimum: 2 ,too_short: "%{count} characters is the maximum allowed" }, allow_nil: true
   validates :from, length: { maximum: 12 ,  too_long: "%{count} characters is the maximum allowed" }, strict: true
   validates :destination, length: { in: 2..12 }, allow_blank: true
-  validates :price, length: { is: 5, wrong_length: "you gave wrong length" }
   validates :price, numericality: {strict: true}
-  validates :price, numericality: {in: 10000..59999, other_than: 29999}
 #  validates :from, uniqueness: {scope: :destination, case_sensitive: false}
   paginates_per 50
 end
