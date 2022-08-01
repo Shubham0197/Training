ce400bde64c900dc697e1be4ff38d07e9e689b7b
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-01 14:38:49 +0530

    Aircraft and employee certified validation created

--- Rails/airport/app/models/schedule.rb
+++ Rails/airport/app/models/schedule.rb
@@ -2,4 +2,17 @@ class Schedule < ApplicationRecord
   belongs_to :flight
   belongs_to :aircraft
   belongs_to :employee
+  validate :pilot_aircraft_certified?
+  
+  private
+  def pilot_aircraft_certified?
+    if (this.employee.certified_ids&& this.aircraft.certified_ids)
+      true
+    else
+      false
+    end
+  end
+
+
+
 end
