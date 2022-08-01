00e161da7c03c110d1d705f8999e1ca9d3fb36a9
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-01 17:59:16 +0530

    added certified belongs to aircraft associations

--- Rails/airport/app/models/certified.rb
+++ Rails/airport/app/models/certified.rb
@@ -4,5 +4,6 @@ class Certified < ApplicationRecord
   #validates :employee_id, numericality: { odd: true, less_than_or_equal_to: 20}
   #validates :aircraft_id, numericality: { even: true, greater_than: 2, only_integer: true}
   #validates :aircraft_id, uniqueness: {scope: :employee_id}
+  belongs_to :aircraft
   paginates_per 50
 end
