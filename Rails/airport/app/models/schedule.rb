294c8c2f8cbc4a60261466641e452aea56b1faa4
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-02 20:19:49 +0530

    added validation in the schedule model and fixed pilot_aircraft_certified? method

--- Rails/airport/app/models/schedule.rb
+++ Rails/airport/app/models/schedule.rb
@@ -2,11 +2,17 @@ class Schedule < ApplicationRecord
   belongs_to :flight
   belongs_to :aircraft
   belongs_to :employee
+  validates :aircraft_id, presence: true
+  validates :employee_id, presence: true
+  validates :flight_id, presence: true
   validate :pilot_aircraft_certified?
+
   
   private
   def pilot_aircraft_certified?
-    if (this.employee.certified_ids&& this.aircraft.certified_ids)
+    aircraft = Aircraft.find(aircraft_id)
+    employee = Employee.find(employee_id)
+    if (employee.certified_ids && aircraft.certified_ids)
       true
     else
       false
