c88c51fec6b52a682d218c07e73ed1434924ae90
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-02 14:00:23 +0530

    Removed duplicate code from the employee controller

--- Rails/airport/app/controllers/employees_controller.rb
+++ Rails/airport/app/controllers/employees_controller.rb
@@ -12,15 +12,11 @@ class EmployeesController < ApplicationController
 
   def create
     @employee = Employee.new(employee_params)
-    @employee.save!
-    if @employee.save!
+    if @employee.save
       CrudNotificationMailer.create_notification(@employee).deliver_later
-      flash[:notice] = 'Aircraft Added'
+      flash[:notice] = 'New Employee Added'
       redirect_to(employees_index_path)
     end
-  end
-
-  def show
   end
 
   def edit
