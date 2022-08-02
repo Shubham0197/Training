e76b5ed4f195abf1b979d2aeedaac44e4c3b0512
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-02 23:57:17 +0530

    Created certifieds create new and update action and Views

--- Rails/airport/app/controllers/certifieds_controller.rb
+++ Rails/airport/app/controllers/certifieds_controller.rb
@@ -5,15 +5,35 @@ class CertifiedsController < ApplicationController
 
   def new
     @certified = Certified.new
+    @aircrafts = Aircraft.pluck(:name, :id)
+    @pilots = Employee.pluck(:name, :id)
   end
 
   def create
+    certified = Certified.new(certified_params)
+    if certified.save
+      flash[:message] = "Pilot has been certified for the Aircraft"
+      redirect_to(certifieds_path)
+    else
+      flash[:alert] = certified.errors.full_messages
+      redirect_to(new_certified_path)
+    end
   end
 
   def edit
+    @certified = Certified.find(params[:id])
+    @aircrafts = Aircraft.pluck(:name, :id)
+    @pilots = Employee.pluck(:name, :id)
   end
 
   def update
+    @certified = Certified.find(params[:id])
+    if @certified.update(certified_params)
+      flash[:notice] = 'Certified Mapping updated'
+      redirect_to(certifieds_path)
+    else
+      render('edit')
+    end
   end
 
   def destroy
@@ -22,6 +42,12 @@ class CertifiedsController < ApplicationController
       flash[:notice] = 'Certifieds destroyed'
       redirect_to(certifieds_path)
     end
+  end
+
+  private
+  
+  def certified_params
+    params.require(:certified).permit( :aircraft_id, :employee_id )
   end
 
 end
