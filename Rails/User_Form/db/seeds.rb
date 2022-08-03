90afbcd21f01c8b1bc16f377b8a4f3a921398be5
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-03 18:40:56 +0530

    Added date to the users table using rails db:seed

--- Rails/User_Form/db/seeds.rb
+++ Rails/User_Form/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  User.create(name: "UserName#{i}", date_of_birth: DateTime.now )
+  print "User Created no. #{i}\n"
+end
