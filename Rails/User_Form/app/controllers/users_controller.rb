2084496d388915ab8152ae5689ad3e7d219e1ee4
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-03 19:46:12 +0530

    Created the action for the new,create, edit and  update operation and added links in the index view for edit and  destroy action

--- Rails/User_Form/app/controllers/users_controller.rb
+++ Rails/User_Form/app/controllers/users_controller.rb
@@ -4,14 +4,41 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user =  User.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to(users_path)
+    else 
+      render :new
+    end
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    if user.save
+      redirect_to(users_path)
+    else 
+      render :edit
+    end 
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to(users_path)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :address, :date_of_birth, :mobile_number, :salary)
   end
 end
