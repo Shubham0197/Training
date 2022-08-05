9004bcdb9b2c3742078a104b308036df1fdfc650
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-05 19:33:35 +0530

    Refactor the Aircraft spec by introducing subject

--- Rails/airport/spec/models/aircraft_spec.rb
+++ Rails/airport/spec/models/aircraft_spec.rb
@@ -1,15 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Aircraft, :type => :model do
+  subject{
+    described_class.new(name: "Airmax", cruising_range: 1300)
+  }
+
   it " is invalid if its cruising range is below 1000" do
-    aircraft = Aircraft.new(name: "Airmax", cruising_range: 1300)
-    cruisable= aircraft.cruise?
+    cruisable= subject.cruise?
     expect(cruisable).to be_truthy
   end
 
+  it "is valid with a name" do
+    expect(subject).to be_valid
+  end
+
   it "is not valid without a name" do
-    aircraft = Aircraft.new(name: "Airmax", cruising_range: 1300)
-    print "Aircraft",aircraft.name
-    expect(@aircraft).to be_valid
+    if subject.name.nil?
+      expect(subject).to_not be_valid
+    end
   end
 end
