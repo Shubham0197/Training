5347398109b03d38792586b400cc947333677604
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-05 17:21:58 +0530

    Installed factory girl gem and deleted test directory from airport project

--- Rails/airport/test/models/certified_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CertifiedTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- Rails/airport/test/models/flights_aircraft_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class FlightsAircraftTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
