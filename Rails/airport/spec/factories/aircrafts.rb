5a1fe8c1d217caffe713e3e3b5bd12e9a5e64c90
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-05 20:22:51 +0530

    Usage of the factory girl gem and using it the rspec test case

--- Rails/airport/spec/factories/aircrafts.rb
+++ Rails/airport/spec/factories/aircrafts.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :aircraft do
-    
+    name "Airmax"
+    cruising_range 1300
   end
 end
--- Rails/airport/spec/models/aircraft_spec.rb
+++ Rails/airport/spec/models/aircraft_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Aircraft, :type => :model do
-  subject{
-    described_class.new(name: "Airmax", cruising_range: 1300)
-  }
+  before do
+    @aircraft = FactoryGirl.build(:aircraft)
+  end
 
   it " is invalid if its cruising range is below 1000" do
-    cruisable= subject.cruise?
+    cruisable= @aircraft.cruise?
     expect(cruisable).to be_truthy
   end
 
   it "is valid with a name" do
-    expect(subject).to be_valid
+    expect(@aircraft).to be_valid
   end
 
   it "is not valid without a name" do
-    if subject.name.nil?
-      expect(subject).to_not be_valid
+    if @aircraft.name.nil?
+      expect(@aircraft).to_not be_valid
     end
   end
 end
