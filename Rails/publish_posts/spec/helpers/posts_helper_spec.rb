806946ee681b56e99960fdf4618b2e86af69ffe9
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-09 18:55:57 +0530

    Created helper test to check its funtionality

--- Rails/publish_posts/spec/helpers/posts_helper_spec.rb
+++ Rails/publish_posts/spec/helpers/posts_helper_spec.rb
@@ -11,5 +11,14 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe PostsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'assigns a user to a post' do
+    creator = User.first_or_create!(email: 'dean@example.com', password: 'password', password_confirmation: 'password')
+    @post = Post.new(
+      title: 'MyString',
+      body: 'MyText',
+      views: 1
+    )
+    assign_post_creator(@post, creator)
+    expect(@post.user).to be(creator)
+  end
 end
