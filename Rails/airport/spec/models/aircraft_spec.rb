cdbc29ea58e7c4ebac77f1a1ab7f27430a508d5b
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-10 16:35:30 +0530

    Creating Custom matchers for the Rspec test cases

--- Rails/airport/spec/models/aircraft_spec.rb
+++ Rails/airport/spec/models/aircraft_spec.rb
@@ -4,6 +4,18 @@ RSpec.describe Aircraft, :type => :model do
   before do
     @aircraft = FactoryGirl.build(:aircraft)
   end
+  let(:expected_aircraft) do
+    Monster.new(
+      name: 'Aircycle',
+      cruising_range: 1300
+    )
+  end
+
+  subject { Aircraft.new(name: 'Aircycle',cruising_range: 1300) }
+
+  it 'creates a monster with the given features'
+    expect(subject).to be_the_same_aircraft_as(:expected_aircraft)
+  end
 
   it " is invalid if its cruising range is below 1000" do
     cruisable= @aircraft.cruise?
