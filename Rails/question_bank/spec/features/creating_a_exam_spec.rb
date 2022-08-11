e72fcf325f61257e6dda68bfa066fb4cbd0e77c9
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-11 16:56:09 +0530

    Integration Testing for creating new exam using feature spec and check it on index page

---
+++ Rails/question_bank/spec/features/creating_a_exam_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "CreatingAExams", type: :feature do
+  scenario 'valid inputs' do
+    visit new_exam_path
+    fill_in 'Name', with: 'Maths'
+    click_on 'Create Exam'
+    visit exams_path
+    expect(page).to have_content('Maths')
+  end
+end
