653cd385d8f892b011c62d804e7ed3bdf11c8caf
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-11 18:08:46 +0530

    Integration test for updating a  exam

--- Rails/question_bank/spec/features/creating_a_exam_spec.rb
+++ Rails/question_bank/spec/features/creating_a_exam_spec.rb
@@ -15,4 +15,13 @@ RSpec.feature "CreatingAExams", type: :feature do
     click_on 'Create Exam'
     expect(page).to have_content("Name can't be blank")
   end
+
+  scenario 'updating via valid inputs' do
+    math = Exam.create!(name: 'Math')
+    visit edit_exam_path(id: math.id)
+    fill_in 'Name', with: 'Mathematics'
+    click_on 'Update Exam'
+    visit exams_path
+    expect(page).to have_content('Mathematics')
+  end
 end
