b3871e6fa9870d2478054a86d32d5e732fcf42ec
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-11 17:36:09 +0530

    Integration test for trying to create a exam with invalid inputs

---
+++ Rails/question_bank/app/models/exam.rb
@@ -0,0 +1,3 @@
+class Exam < ApplicationRecord
+  validates_presence_of :name
+end
--- Rails/question_bank/spec/features/creating_a_exam_spec.rb
+++ Rails/question_bank/spec/features/creating_a_exam_spec.rb
@@ -8,4 +8,11 @@ RSpec.feature "CreatingAExams", type: :feature do
     visit exams_path
     expect(page).to have_content('Maths')
   end
+
+  scenario 'invalid inputs' do
+    visit new_exam_path
+    fill_in 'Name', with: ''
+    click_on 'Create Exam'
+    expect(page).to have_content("Name can't be blank")
+  end
 end
