410cc6edec15e27a2006754475d56fd15bbf43d4
Shubham0197 <shubhamjain0197@gmail.com>
2022-08-19 12:53:22 +0530

    User devise and Letter opener gem installed in grocery_shop application

---
+++ Rails/grocery_shop/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_presence_of :password
+end
